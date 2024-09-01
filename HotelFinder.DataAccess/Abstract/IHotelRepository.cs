26c0ffd07cb2605deccedfdd4b7f19e736e0fb58
seymafatma02 <167695027+seymafatma02@users.noreply.github.com>
2024-09-01 19:51:09 +0300

    Add IHotelRepository interface with CRUD operations for hotels

---
+++ HotelFinder.DataAccess/Abstract/IHotelRepository.cs
@@ -0,0 +1,25 @@
+﻿using HotelFinder.Entities;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace HotelFinder.DataAccess.Abstract
+{//IHotelRepository, otel verileriyle ilgili işlemleri tanımlayan bir arayüzdür.
+ //Bu arayüzü uygulayan bir sınıf, belirli CRUD işlemlerini sağlamak zorundadır.
+ 
+    public interface IHotelRepository
+    {
+
+        List<Hotel> GetAllHotels();//Tüm otellerin listesini döndüren bir metot.
+
+        Hotel GetHotelById(int id);//Belirtilen id değerine sahip tek bir oteli döndüren bir metot.
+
+        Hotel CreateHotel(Hotel hotel);//Yeni bir otel oluşturup veritabanına ekleyen bir metot. Oluşturulan otel nesnesini geri döndürür.
+
+        Hotel UpdateHotel(Hotel hotel);//Mevcut bir oteli güncelleyen bir metot. Güncellenmiş otel nesnesini geri döndürür.
+
+        void DeleteHotel(int id);//Belirtilen id değerine sahip oteli veritabanından silen bir metot.
+    }
+}
